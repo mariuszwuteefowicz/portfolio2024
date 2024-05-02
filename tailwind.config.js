f4efebc613da01fb349ddc3411e3180c656cbb53
Mariusz <mariusz.c008@protonmail.com>
2024-05-02 17:46:26 +0200

    add styles for the skills section on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
       },
       maxWidth: {
             'wtf': '82rem'
+      },
+      borderRadius: {
+        'wtf': '0.625rem'
+      },
+      boxShadow: {
+        'innerlight': 'inset 0 2px 0 0 rgba(255,255,255,0.15)'
       }
     },
   },
